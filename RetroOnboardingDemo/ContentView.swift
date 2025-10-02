156b0e2c5bdab37ff7d10f48127b6b99006d4577
George Cartridge <35803468+georgecartridge@users.noreply.github.com>
2025-10-02 15:17:52 +0100

    Initial Commit

---
+++ RetroOnboardingDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RetroOnboardingDemo
+//
+//  Created by George on 02/10/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RetroOnboardingDemo/RetroOnboardingDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  RetroOnboardingDemoApp.swift
+//  RetroOnboardingDemo
+//
+//  Created by George on 02/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct RetroOnboardingDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
